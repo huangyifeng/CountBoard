76af91e4e96742a23d40965985de32eee882dcb8
huangyifeng <huangyifeng@huangmatoMac-mini.local>
2012-02-25 15:37:06 +0800

    Initial Commit

---
+++ CountBoard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CountBoard
+//
+//  Created by yifeng huang on 12-2-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
