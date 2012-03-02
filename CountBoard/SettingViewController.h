b7d87e1df08739aa1c52698a16465b3f092e88ed
huangyifeng <luckyJerry85@gmail.com>
2012-03-02 18:35:44 +0800

    add a setting page

---
+++ CountBoard/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  CountBoard
+//
+//  Created by Huang YiFeng on 12-3-2.
+//  Copyright (c) 2012年 Cybozu, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
